9edf122de473d531aa485e8fb10044588b03555e
Peter Bright <drpizza@quiscalusmexicanus.org>
2012-04-13 05:26:28 +0100

    reduce indent, use auto

--- DupeHunter/src/DupeHunter.cpp
+++ DupeHunter/src/DupeHunter.cpp
@@ -6,9 +6,9 @@
 typedef std::map<unsigned __int64, std::vector<std::wstring> > size_map_type;
 
 bool permitted_name(const std::wstring& name, const std::vector<boost::wregex>& include_patterns, const std::vector<boost::wregex>& exclude_patterns) {
-	for(std::vector<boost::wregex>::const_iterator ipit(include_patterns.begin()), ipend(include_patterns.end()); ipit != ipend; ++ipit) {
+	for(auto ipit(include_patterns.cbegin()), ipend(include_patterns.cend()); ipit != ipend; ++ipit) {
 		if(boost::regex_match(name, *ipit)) {
-			for(std::vector<boost::wregex>::const_iterator epit(exclude_patterns.begin()), epend(exclude_patterns.end()); epit != epend; ++epit) {
+			for(auto epit(exclude_patterns.cbegin()), epend(exclude_patterns.cend()); epit != epend; ++epit) {
 				if(boost::regex_match(name, *epit)) {
 					return false;
 				}
@@ -39,7 +39,7 @@ unsigned __int64 populate_files(size_map_type& files, const std::vector<boost::w
 	const std::wstring search_path(basePath + (basePath[basePath.size() - 1] == L'\\' ? L"*" : L"\\*"));
 	WIN32_FIND_DATAW found = {0};
 	HANDLE finder(::FindFirstFileW(search_path.c_str(), &found));
-	ON_BLOCK_EXIT([=]() { ::FindClose(finder); });
+	ON_BLOCK_EXIT([=] { ::FindClose(finder); });
 	do {
 		static const wchar_t* dot(L".");
 		static const wchar_t* dotdot(L"..");
@@ -88,7 +88,7 @@ std::vector<std::vector<std::wstring> > n_way_compare(unsigned __int64 file_size
 		throw std::exception("Buffer too small for this number of files");
 	}
 	static const unsigned __int64 sector_size(4096); // TODO get the right size
-	const unsigned __int64 rounded_file_size = round_to_next_multiple(file_size, sector_size);
+	const unsigned __int64 rounded_file_size(round_to_next_multiple(file_size, sector_size));
 	const bool aligned_reads    = (static_cast<unsigned __int64>(names.size()) * sector_size      ) <= static_cast<unsigned __int64>(total_buffer_size);
 	const bool read_whole_files = (static_cast<unsigned __int64>(names.size()) * rounded_file_size) <= static_cast<unsigned __int64>(total_buffer_size);
 	const unsigned __int64 buffer_size = aligned_reads ? (read_whole_files ? rounded_file_size
@@ -180,111 +180,108 @@ std::vector<std::vector<std::wstring> > n_way_compare(unsigned __int64 file_size
 }
 
 int wmain(int argc, wchar_t* argv[])
-{
-	try
-	{
-		namespace po = boost::program_options;
+try {
+	namespace po = boost::program_options;
 
-		size_t buffer_size(0);
-		std::vector<std::wstring> directories;
-		std::vector<std::wstring> inc_patterns;
-		std::vector<std::wstring> inc_epatterns;
-		std::vector<std::wstring> exc_patterns;
-		std::vector<std::wstring> exc_epatterns;
+	size_t buffer_size(0);
+	std::vector<std::wstring> directories;
+	std::vector<std::wstring> inc_patterns;
+	std::vector<std::wstring> inc_epatterns;
+	std::vector<std::wstring> exc_patterns;
+	std::vector<std::wstring> exc_epatterns;
 
-		po::options_description desc("Allowed options");
-		desc.add_options()
-			("help",                                                                             "show this message")
-			("buffer-size", po::wvalue<size_t>(&buffer_size)->default_value(1024 * 1024 * 1024), "set maximum buffer size")
-			("source",      po::wvalue<std::vector<std::wstring> >(&directories)->composing(),   "directories to search")
-			("include,i",   po::wvalue<std::vector<std::wstring> >(&inc_patterns)->composing(),  "wildcard filename pattern to include")
-			("einclude,I",  po::wvalue<std::vector<std::wstring> >(&inc_epatterns)->composing(), "regex filename pattern to include")
-			("exclude,x",   po::wvalue<std::vector<std::wstring> >(&exc_patterns)->composing(),  "wildcard filename pattern to exclude")
-			("eexclude,X",  po::wvalue<std::vector<std::wstring> >(&exc_epatterns)->composing(), "regex filename pattern to exclude")
-		;
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help",                                                                             "show this message")
+		("buffer-size", po::wvalue<size_t>(&buffer_size)->default_value(1024 * 1024 * 1024), "set maximum buffer size")
+		("source",      po::wvalue<std::vector<std::wstring> >(&directories)->composing(),   "directories to search")
+		("include,i",   po::wvalue<std::vector<std::wstring> >(&inc_patterns)->composing(),  "wildcard filename pattern to include")
+		("einclude,I",  po::wvalue<std::vector<std::wstring> >(&inc_epatterns)->composing(), "regex filename pattern to include")
+		("exclude,x",   po::wvalue<std::vector<std::wstring> >(&exc_patterns)->composing(),  "wildcard filename pattern to exclude")
+		("eexclude,X",  po::wvalue<std::vector<std::wstring> >(&exc_epatterns)->composing(), "regex filename pattern to exclude")
+	;
 
-		po::positional_options_description p;
-		p.add("source", -1);
+	po::positional_options_description p;
+	p.add("source", -1);
 
-		po::variables_map vm;
-		po::store(po::wcommand_line_parser(argc, argv).options(desc).style(po::command_line_style::unix_style).positional(p).run(), vm);
-		po::notify(vm);
+	po::variables_map vm;
+	po::store(po::wcommand_line_parser(argc, argv).options(desc).style(po::command_line_style::unix_style).positional(p).run(), vm);
+	po::notify(vm);
 
-		if(vm.count("help")) {
-			std::cout << desc << std::endl;
-			return 1;
-		}
-
-		if(!vm.count("source")) {
-			std::cerr << desc << std::endl;
-			return 1;
-		}
-
-		void* buffer(::VirtualAlloc(nullptr, buffer_size,  MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE));
-		ON_BLOCK_EXIT([=] { ::VirtualFree(buffer, 0, MEM_RELEASE); });
-
-		if(inc_patterns.size() == 0 && inc_epatterns.size() == 0) {
-			inc_patterns.push_back(L"*");
-		}
-
-		std::vector<boost::wregex> include_patterns;
-		std::vector<boost::wregex> exclude_patterns;
-
-		const boost::wregex wildcard_transform(L"([+{}()\\[\\]$\\^|])|(\\*)|(\\?)|(\\.)|([\\\\/:])");
-		// this has to be double-escaped because we want the result to be properly escaped to produce a regex itself.
-		// It would be nice to have raw strings!
-		const std::wstring wildcard_replacement(L"(?1\\\\$1)(?2[^\\\\\\\\/\\:]*)(?3[^\\\\\\\\/\\:])(?4\\(\\?\\:\\\\.|$\\))(?5[\\\\\\\\\\\\/\\:])");
-
-		for(std::vector<std::wstring>::const_iterator it(inc_patterns.begin()), end(inc_patterns.end()); it != end; ++it) {
-			include_patterns.push_back(boost::wregex(boost::regex_replace(*it, wildcard_transform, wildcard_replacement, boost::format_all), boost::regex::icase));
-		}
-		for(std::vector<std::wstring>::const_iterator it(exc_patterns.begin()), end(exc_patterns.end()); it != end; ++it) {
-			exclude_patterns.push_back(boost::wregex(boost::regex_replace(*it, wildcard_transform, wildcard_replacement, boost::format_all), boost::regex::icase));
-		}
-		for(std::vector<std::wstring>::const_iterator it(inc_epatterns.begin()), end(inc_epatterns.end()); it != end; ++it) {
-			include_patterns.push_back(boost::wregex(*it, boost::regex::icase));
-		}
-		for(std::vector<std::wstring>::const_iterator it(exc_epatterns.begin()), end(exc_epatterns.end()); it != end; ++it) {
-			exclude_patterns.push_back(boost::wregex(*it, boost::regex::icase));
-		}
-
-		size_map_type files;
-		unsigned __int64 total_files(0);
-
-		for(std::vector<std::wstring>::const_iterator it(directories.begin()), end(directories.end()); it != end; ++it) {
-			std::wcout << L"Searching " << *it << std::endl;
-			total_files += populate_files(files, include_patterns, exclude_patterns, *it);
-		}
-
-		std::wcout << L"Found " << total_files << L" files matching search criteria" << std::endl;
-		unsigned __int64 files_read(total_files);
-		for(size_map_type::iterator it(files.begin()), end(files.end()); it != end;) {
-			if(it->first == 0 || it->second.size() == 1) {
-				files_read -= it->second.size();
-				files.erase(it++);
-			}
-			else {
-				++it;
-			}
-		}
-		std::wcout << L"Comparing " << files_read << L" files with non-unique sizes" << std::endl;
-		for(size_map_type::iterator it(files.begin()), end(files.end()); it != end; ++it) {
-			std::wcout << L"Comparing " << it->second.size() << L" files of size " << it->first << std::endl;
-			std::vector<std::vector<std::wstring> > duplicates(n_way_compare(it->first, it->second, buffer, buffer_size));
-			size_t count(0);
-			for(std::vector<std::vector<std::wstring> >::const_iterator it(duplicates.begin()), end(duplicates.end()); it != end; ++it) {
-				std::wcout << L"\tDuplicate set " << ++count << std::endl;
-				for(std::vector<std::wstring>::const_iterator nit(it->begin()), nend(it->end()); nit != nend; ++nit) {
-					std::wcout << L"\t\t" << *nit << std::endl;
-				}
-			}
-		}
-
-		return 0;
+	if(vm.count("help")) {
+		std::cout << desc << std::endl;
+		return 1;
 	}
-	catch(std::exception& e) {
-		std::cerr << "Caught exception" << std::endl;
-		std::cerr << e.what() << std::endl;
-		return -2;
+
+	if(!vm.count("source")) {
+		std::cerr << desc << std::endl;
+		return 1;
 	}
+
+	void* buffer(::VirtualAlloc(nullptr, buffer_size,  MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE));
+	ON_BLOCK_EXIT([=] { ::VirtualFree(buffer, 0, MEM_RELEASE); });
+
+	if(inc_patterns.size() == 0 && inc_epatterns.size() == 0) {
+		inc_patterns.push_back(L"*");
+	}
+
+	std::vector<boost::wregex> include_patterns;
+	std::vector<boost::wregex> exclude_patterns;
+
+	const boost::wregex wildcard_transform(L"([+{}()\\[\\]$\\^|])|(\\*)|(\\?)|(\\.)|([\\\\/:])");
+	// this has to be double-escaped because we want the result to be properly escaped to produce a regex itself.
+	// It would be nice to have raw strings!
+	const std::wstring wildcard_replacement(L"(?1\\\\$1)(?2[^\\\\\\\\/\\:]*)(?3[^\\\\\\\\/\\:])(?4\\(\\?\\:\\\\.|$\\))(?5[\\\\\\\\\\\\/\\:])");
+
+	for(auto it(inc_patterns.cbegin()), end(inc_patterns.cend()); it != end; ++it) {
+		include_patterns.push_back(boost::wregex(boost::regex_replace(*it, wildcard_transform, wildcard_replacement, boost::format_all), boost::regex::icase));
+	}
+	for(auto it(exc_patterns.cbegin()), end(exc_patterns.cend()); it != end; ++it) {
+		exclude_patterns.push_back(boost::wregex(boost::regex_replace(*it, wildcard_transform, wildcard_replacement, boost::format_all), boost::regex::icase));
+	}
+	for(auto it(inc_epatterns.cbegin()), end(inc_epatterns.cend()); it != end; ++it) {
+		include_patterns.push_back(boost::wregex(*it, boost::regex::icase));
+	}
+	for(auto it(exc_epatterns.cbegin()), end(exc_epatterns.cend()); it != end; ++it) {
+		exclude_patterns.push_back(boost::wregex(*it, boost::regex::icase));
+	}
+
+	size_map_type files;
+	unsigned __int64 total_files(0);
+
+	for(auto it(directories.cbegin()), end(directories.cend()); it != end; ++it) {
+		std::wcout << L"Searching " << *it << std::endl;
+		total_files += populate_files(files, include_patterns, exclude_patterns, *it);
+	}
+
+	std::wcout << L"Found " << total_files << L" files matching search criteria" << std::endl;
+	unsigned __int64 files_read(total_files);
+	for(auto it(files.cbegin()), end(files.cend()); it != end;) {
+		if(it->first == 0 || it->second.size() == 1) {
+			files_read -= it->second.size();
+			files.erase(it++);
+		}
+		else {
+			++it;
+		}
+	}
+	std::wcout << L"Comparing " << files_read << L" files with non-unique sizes" << std::endl;
+	for(auto it(files.begin()), end(files.end()); it != end; ++it) {
+		std::wcout << L"Comparing " << it->second.size() << L" files of size " << it->first << std::endl;
+		std::vector<std::vector<std::wstring> > duplicates(n_way_compare(it->first, it->second, buffer, buffer_size));
+		size_t count(0);
+		for(auto it(duplicates.cbegin()), end(duplicates.cend()); it != end; ++it) {
+			std::wcout << L"\tDuplicate set " << ++count << std::endl;
+			for(std::vector<std::wstring>::const_iterator nit(it->begin()), nend(it->end()); nit != nend; ++nit) {
+				std::wcout << L"\t\t" << *nit << std::endl;
+			}
+		}
+	}
+
+	return 0;
+}
+catch(std::exception& e) {
+	std::cerr << "Caught exception" << std::endl;
+	std::cerr << e.what() << std::endl;
+	return -2;
 }
